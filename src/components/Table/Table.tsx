f188b88778dd7a43c3b4a286143e6facf84c8343
wecoastca <foxconq@gmail.com>
2020-01-28 21:51:43 +0300

    Add func for dynamic col num

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -9,39 +9,38 @@ type States = {
     data: any
 }
 
-export class Table extends React.Component<Props,States>{
-    constructor(props:Props) {
+export class Table extends React.Component<Props, States>{
+    constructor(props: Props) {
         super(props);
-        
+
         this.state = {
             data: info,
 
         }
     }
-    renderTableData(){
-        
-        return this.state.data.map((field, index)=>{
-            const {Name,Email,Phone,Company} = field;
+    renderTableData() {
 
-            return(
+        return this.state.data.map((field, index) => {
+            let columns = Object.keys(field);
+
+            return (
                 <tr key={index}>
-                    <td>{Name}</td>
-                    <td>{Email}</td>
-                    <td>{Phone}</td>
-                    <td>{Company}</td>
+                    {columns.map((value, i) => {
+                        return <td key={i}>{field[columns[i]]}</td>
+                    })}
                 </tr>
             )
         })
     }
 
-    renderTableHeader(){
+    renderTableHeader() {
         let headerKeys = Object.keys(this.state.data[0]);
-        return headerKeys.map((value,index)=>{
+        return headerKeys.map((value, index) => {
             return <th key={index}>{value.toUpperCase()}</th>
         })
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="table-wrapper">
                 <h1 className="title">Amazing Hiring Test Table</h1>
                 <table className="table-content">
